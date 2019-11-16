a3b927c248c7ff7fe2f2fda14469205c38c6e1df
Jake <jakercrabtree@utexas.edu>
2019-11-16 15:25:55 -0600

    levelspawner

---
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform topRight;
+    [SerializeField] private Transform bottomLeft;
+    [SerializeField] private float stepSize;
+    [SerializeField] private int numObjects;
+
+    [SerializeField] private GameObject testObj;
+
+    public void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        Vector3 topRightPos = topRight.position;
+        Vector3 bottomLeftPos = bottomLeft.position;
+        int numXPos = Mathf.RoundToInt((topRightPos.x - bottomLeftPos.x) / stepSize);
+        int numYPos = Mathf.RoundToInt((topRightPos.y - bottomLeftPos.y) / stepSize);
+        Debug.Log("xpos " + numXPos);
+        Debug.Log("ypos " + numYPos);
+        for (int i = 0; i < numXPos; ++i)
+        {
+            for (int j = 0; j < numYPos; ++j)
+            {
+                Instantiate(testObj, new Vector3(i * stepSize, j * stepSize, topRightPos.z), Quaternion.identity);
+            }
+        }
+    }
+}
