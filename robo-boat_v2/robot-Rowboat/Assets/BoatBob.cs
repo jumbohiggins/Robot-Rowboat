9f1315f8b681044830b15fa68a048524dd66382c
jumbohiggins <jumbohiggins@gmail.com>
2019-11-16 15:16:29 -0600

    Boat with linear drive oars in low poly water
    Boat with linear drive oars in low poly water

---
+++ robo-boat_v2/robot-Rowboat/Assets/BoatBob.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatBob : MonoBehaviour {
+
+
+    public float freq;
+    public float amp;
+    public Renderer rend;
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    // Use this for initialization
+    void Start () {
+        //renderer.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+        rend = GetComponent<Renderer>();
+        rend.material.mainTexture.wrapMode = TextureWrapMode.Repeat;
+        freq = Random.Range(freq - 0.5f, freq + .5f);
+        amp = Random.Range(amp - .5f, amp + .5f);
+        posOffset = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        tempPos = posOffset;
+
+        transform.position = new Vector3(gameObject.transform.position.x, tempPos.y + Mathf.Sin(Time.fixedTime * Mathf.PI * freq) * amp, gameObject.transform.position.z);
+
+    }
+}
