e77af4eb9b4da8be47f77b4dd22aae58c3098709
Jake <jakercrabtree@utexas.edu>
2019-11-16 18:56:10 -0600

    spawning and programmer art

--- robo-boat_v2/robot-Rowboat/Assets/Scripts/MapGenerator.cs
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/MapGenerator.cs
@@ -2,35 +2,126 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class MapGenerator : MonoBehaviour
 {
+    // MapGeneration
     [SerializeField] private Transform topRight;
     [SerializeField] private Transform bottomLeft;
     [SerializeField] private float stepSize;
     [SerializeField] private int numObjects;
-
-    [SerializeField] private GameObject testObj;
-
-    public void Start()
+    [SerializeField] private List<GameObject> islandObjects;
+    
+    //Events
+    [SerializeField] private List<GameObject> eventObjects;
+    [SerializeField] private float roundTime;
+    [SerializeField] private int beginningBufferEvents;
+    [SerializeField] private int numEvents;
+    [SerializeField] private Transform player;
+    
+    private float currTime;
+    private bool gameOver;
+    
+    private HashSet<int> spawnedPositions = new HashSet<int>();
+    
+    private void Start()
     {
-        Spawn();
+        GenerateMap();
+        currTime = roundTime;
+        StartCoroutine(GenerateEvents());
     }
 
-    public void Spawn()
+    private void Update()
+    {
+        currTime -= Time.deltaTime;
+        if (currTime <= 0f)
+        {
+            gameOver = true;
+            //TODO end game
+        }
+    }
+
+    public void GenerateMap()
+    {
+        for (int i = 0; i < numObjects; ++i)
+        {
+            SpawnRandom(PickIslandObject());
+        }
+    }
+
+    public IEnumerator GenerateEvents()
+    {
+        float eventStepSize = roundTime / (beginningBufferEvents + numEvents);
+        float currSlot = eventStepSize * beginningBufferEvents;
+        yield return new WaitForSeconds(currSlot);
+        while (!gameOver)
+        {
+            float time = UnityEngine.Random.Range(currSlot, currSlot + eventStepSize);
+            currSlot += eventStepSize;
+            Invoke(nameof(SpawnEventAtPlayer), time);
+            yield return new WaitForSeconds(eventStepSize);
+        }
+    }
+    
+    public void SpawnEventAtPlayer()
+    {
+        GameObject toSpawn = PickEvent();
+        Instantiate(toSpawn, player.position, toSpawn.transform.rotation);
+    }
+    
+    public List<int> GetNeighbors(int x, int y, int xMax, int yMax)
+    {
+        List<int> res = new List<int>();
+        for (int i = -1; i <= 1; ++i)
+        {
+            for (int j = -1; j <= 1; ++j)
+            {
+                if (x == 0 && y == 0) continue;
+                int newX = x + i;
+                int newY = y + j;
+                if (newX < xMax &&
+                    newX >= 0 &&
+                    newY < yMax &&
+                    newY >= 0)
+                {
+                    res.Add(x*xMax + y);
+                }
+                
+            }
+        }
+        return res;
+    }
+
+    public void SpawnRandom(GameObject objToSpawn)
     {
         Vector3 topRightPos = topRight.position;
         Vector3 bottomLeftPos = bottomLeft.position;
         int numXPos = Mathf.RoundToInt((topRightPos.x - bottomLeftPos.x) / stepSize);
-        int numYPos = Mathf.RoundToInt((topRightPos.y - bottomLeftPos.y) / stepSize);
-        Debug.Log("xpos " + numXPos);
-        Debug.Log("ypos " + numYPos);
-        for (int i = 0; i < numXPos; ++i)
+        int numZPos = Mathf.RoundToInt((topRightPos.z - bottomLeftPos.z) / stepSize);
+        float xDiff = (topRightPos.x - bottomLeftPos.x - stepSize * numXPos)/2f;
+        float zDiff = (topRightPos.z - bottomLeftPos.z - stepSize * numZPos)/2f;
+        
+        int rand;
+        do
         {
-            for (int j = 0; j < numYPos; ++j)
-            {
-                Instantiate(testObj, new Vector3(i * stepSize, j * stepSize, topRightPos.z), Quaternion.identity);
-            }
-        }
+            rand = UnityEngine.Random.Range(0, numXPos * numZPos);
+        } while (spawnedPositions.Contains(rand));
+        spawnedPositions.Add(rand);
+        
+        int x = rand / numXPos;
+        int y = rand % numZPos;
+        Instantiate(objToSpawn,  new Vector3(-1f*xDiff + bottomLeftPos.x + x * stepSize, topRightPos.y, -1f*zDiff + bottomLeftPos.z + y * stepSize), Quaternion.identity);
     }
+
+    public GameObject PickIslandObject()
+    {
+        return islandObjects[UnityEngine.Random.Range(0, islandObjects.Count)];
+    }
+    
+    public GameObject PickEvent()
+    {
+        return eventObjects[UnityEngine.Random.Range(0, eventObjects.Count)];
+    }
+    
 }
---
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/TerriblePlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerriblePlayerController : MonoBehaviour
+{
+
+   [SerializeField] private float speed = 1;
+   public void Update()
+   {
+      transform.position +=
+         new Vector3(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 
+            0, 
+            Input.GetAxisRaw("Vertical") * speed *Time.deltaTime);
+   }
+}
