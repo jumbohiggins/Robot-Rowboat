a7a439621b62854b7082301dc7502eb5c5d10941
jumbohiggins <jumbohiggins@gmail.com>
2019-11-17 15:02:52 -0600

    RobotHands

---
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/BallReturnerL.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallReturnerL : MonoBehaviour {
+
+    public SteamVR_Action_Boolean grabPinch;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.LeftHand;
+    public Transform ReturnPoint;    
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabtgg;
+
+    private Valve.VR.InteractionSystem.Hand hand;
+
+
+    private void Start()
+    {
+        grabtgg.AddOnStateDownListener(TriggerHeld, handType);
+        grabtgg.AddOnStateDownListener(TriggerUp, handType);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is up");
+        this.gameObject.transform.position = ReturnPoint.transform.position;
+        //this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void TriggerHeld(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");        
+        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
---
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/BallReturnerR.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallReturnerR : MonoBehaviour {
+
+    public SteamVR_Action_Boolean grabPinch;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.RightHand;
+    public Transform ReturnPoint;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabtgg;
+
+    private Valve.VR.InteractionSystem.Hand hand;
+
+
+    private void Start()
+    {
+        grabtgg.AddOnStateDownListener(TriggerHeld, handType);
+        grabtgg.AddOnStateDownListener(TriggerUp, handType);
+        //SteamVR_Input_ActionSet_Rowboat
+        //.AddOnStateDownListener(TriggerDown, handType);
+        //TriggerDown.AddOnStateUpListener(TriggerUp, handType);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is up");
+        this.gameObject.transform.position = ReturnPoint.transform.position;
+        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+    }
+
+    public void TriggerHeld(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        Debug.Log("Trigger is down");        
+        this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            this.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+        }
+    }
+}
