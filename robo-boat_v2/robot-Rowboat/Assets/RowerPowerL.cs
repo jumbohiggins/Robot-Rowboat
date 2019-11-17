5f8fc40b297127d46921f1b8d0aa02742c842925
jumbohiggins <jumbohiggins@gmail.com>
2019-11-16 21:08:56 -0600

    Pushing mostly working rowboat
    sdaklfj

---
+++ robo-boat_v2/robot-Rowboat/Assets/RowerPowerL.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowerPowerL : MonoBehaviour {
+    public Transform lastpos;
+    private Vector3 vthrust;
+    public Rigidbody boat;
+    public float m_speed = 5.0f;
+    public bool wdir;
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Debug.Log("ON");
+        Debug.Log(this.gameObject.transform.position.y);
+
+      
+        if (this.gameObject.transform.position.y < 1)
+        {
+            Debug.Log("In water");
+            boat.velocity = -transform.forward * m_speed;
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_speed, Space.World);
+        }
+
+	}
+}
---
+++ robo-boat_v2/robot-Rowboat/Assets/RowerPowerR.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RowerPowerR : MonoBehaviour {
+    public Transform lastpos;
+    private Vector3 vthrust;
+    public Rigidbody boat;
+    public float m_speed = 5.0f;
+    public bool wdir;
+
+	// Use this for initialization
+	void Start () {
+        lastpos = this.gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.gameObject.transform.position.y < 0)
+        {
+            Debug.Log("In water");
+            Vector3 direction = boat.transform.position + transform.position;
+            vthrust = 1.1f * (lastpos.position - this.gameObject.transform.position);
+            vthrust *= 1.1f * -this.gameObject.transform.position.y;
+            var outthrust = 1 / Mathf.Tan(90);
+            boat.velocity = -transform.forward * m_speed;
+            Debug.Log(wdir);
+            transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * m_speed, Space.World);
+        }
+
+	}
+}
