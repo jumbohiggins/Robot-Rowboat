76ea317be9a04e10467dc0ec4411e226ed5e55a0
Jake Crabtree <jakercrabtree@gmail.com>
2019-11-17 17:02:13 -0600

    hover birds

--- robo-boat_v2/robot-Rowboat/Assets/EvilBird.cs
+++ robo-boat_v2/robot-Rowboat/Assets/EvilBird.cs
@@ -1,17 +1,82 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using NUnit.Framework.Constraints;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EvilBird : Enemy
 {
+
+    [SerializeField] private float speed;
+    [SerializeField] private float jitterSpeed;
+    private Vector3 target;
+    private Vector3 currOffset;
+    private Vector3 oldPos;
+    
     protected override void Die()
     {
         base.Die();
     }
 
-    private void AttackPlayer()
+
+    protected override void EnemyStart()
     {
-        
+        StartCoroutine(MoveAndAttack());
+        target = transform.position;
+        oldPos = target;
+        Debug.Log("init target" + target);
+    }
+
+    public void Update()
+    {
+        float step = speed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, target + currOffset, step);
+        Debug.Log("target" + target);
+        LookAtPlayer();
+        // Check if the position of the cube and sphere are approximately equal.
+        if (Vector3.Distance(transform.position, target) < 0.001f)
+        {
+            target = oldPos;
+        }
+    }
+
+    private IEnumerator MoveAndAttack()
+    {
+        int count = 0;
+        while (true)
+        {
+            Movement();
+            count++;
+            yield return new WaitForSeconds(0.8f + Random.Range(-0.1f, 0.4f));
+        }
+    }
+
+
+
+    private void Movement()
+    {
+        Transform playerTransform = MapGenerator.Instance.player;
+        Vector3 randomPos = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f), Random.Range(-1f,1f));
+        float scaleFactor =
+            jitterSpeed;// * Vector3.Dot((playerTransform.position - transform.position).normalized,randomPos.normalized);
+        Vector3 toAdd = scaleFactor * randomPos;
+        currOffset = toAdd;
+    }
+
+    private void AttackPlayer() 
+    {
+        Debug.Log("attacking");
+        Transform playerTransform = MapGenerator.Instance.player;
+        Vector3 diff = playerTransform.position - transform.position;
+        oldPos = target;
+        target += 0.66f * diff;
+    }
+
+    private void LookAtPlayer()
+    {
+        Transform playerTransform = MapGenerator.Instance.player;
+        transform.LookAt(playerTransform);
     }
     
 }
