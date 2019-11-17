e2d4e1c0517679dddae742c5668111c204a62b43
jumbohiggins <jumbohiggins@gmail.com>
2019-11-16 18:15:37 -0600

    Working Oars
    Uggggg

---
+++ robo-boat_v2/robot-Rowboat/Assets/BallReturnerL.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallReturnerL : MonoBehaviour {
+
+    public SteamVR_Action_Boolean grabPinch;
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.RightHand;
+    public Transform ReturnPoint;
+
+    private Valve.VR.InteractionSystem.Hand hand;
+
+
+    private void Start()
+    {
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+    }
+
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        this.gameObject.transform.position = ReturnPoint.transform.position;
+        
+    }
+
+
+
+}
