33855e2970e24027b3412b0e76a575f2851988a3
jumbohiggins <jumbohiggins@gmail.com>
2019-11-17 19:42:08 -0600

    Pushing fixes
    lkasdfj

--- robo-boat_v2/robot-Rowboat/Assets/BoatHealth.cs
+++ robo-boat_v2/robot-Rowboat/Assets/BoatHealth.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BoatHealth : MonoBehaviour {
 
     public int Health;
+    [SerializeField] private Image image;
+    [SerializeField] private List<Sprite> sprites;
 
 	// Use this for initialization
 	void Start () {
@@ -21,9 +25,14 @@ public class BoatHealth : MonoBehaviour {
         if (col.gameObject.tag == "Enemy")
         {
             Health = Health - 1;
+            image.sprite = sprites[5 - Health];
             Destroy(col.gameObject);
             Debug.Log("DAMAGE");
             Debug.Log(col.gameObject.name);
+            if (Health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
         }
         if (col.gameObject.tag == "Island")
         {
--- robo-boat_v2/robot-Rowboat/Assets/Scripts/PaddleController.cs
+++ robo-boat_v2/robot-Rowboat/Assets/Scripts/PaddleController.cs
@@ -9,13 +9,32 @@ public class PaddleController : MonoBehaviour {
     public Rigidbody boat;
     public float torque;
     public float m_speed = 5.0f;
+    public Vector3 initPos;
+    public Quaternion initRot;
+    public GameObject[] balls;
+    private List<Vector3> ballPos;
+    private List<Quaternion> ballRots;
     // Use this for initialization
-    void Start () {
-		
+    void Start() {
+        initPos = boat.transform.position;
+        initRot = boat.transform.rotation;
+        foreach (GameObject ball in balls){
+            ballPos.Add(ball.transform.position);
+            ballRots.Add(ball.transform.rotation);
+        }
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ResetBoat();
+
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
         if (leftPaddle.gameObject.transform.position.y < 1 && rightPaddle.gameObject.transform.position.y > 1)
         {
@@ -23,7 +42,7 @@ public class PaddleController : MonoBehaviour {
             //boat.velocity = -transform.forward * m_speed;
             //transform.Rotate(new Vector3(0, -5, 0) * Time.deltaTime * m_speed, Space.World);
             float turn = Input.GetAxis("Horizontal");
-            boat.AddTorque(transform.up * torque * -1.0f);
+            boat.AddTorque(transform.up * torque * -2.0f);
         }
 
         if (rightPaddle.gameObject.transform.position.y < 1 && leftPaddle.gameObject.transform.position.y > 1)
@@ -32,13 +51,27 @@ public class PaddleController : MonoBehaviour {
             //boat.velocity = -transform.forward * m_speed;
             //transform.Rotate(new Vector3(0, 5, 0) * Time.deltaTime * m_speed, Space.World);
             float turn = Input.GetAxis("Horizontal");
-            boat.AddTorque(transform.up * torque * 1.0f);
+            boat.AddTorque(transform.up * torque * 2.0f);
         }
         if (leftPaddle.gameObject.transform.position.y < 1 && rightPaddle.gameObject.transform.position.y < 1)
         {
             //Debug.Log("Going Straiht");
             boat.velocity = -transform.forward * m_speed;
             //transform.Rotate(new Vector3(0, 5, 0) * Time.deltaTime * m_speed, Space.World);
+        }
+    }
+
+    void ResetBoat()
+    {
+        Debug.Log("reset");
+        boat.transform.rotation = initRot;
+        boat.transform.position = new Vector3(boat.transform.position.x, initPos.y, boat.transform.position.z);
+        int i = 0;
+        foreach(GameObject ball in balls)
+        {
+            ball.transform.position = ballPos[i];
+            ball.transform.rotation = ballRots[i];
+            ++i;
         }
     }
 }
