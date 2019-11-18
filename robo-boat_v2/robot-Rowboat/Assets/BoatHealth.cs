a992dd5f6419c207227b4fb3bfbbd81c44f7c6ce
jumbohiggins <jumbohiggins@gmail.com>
2019-11-17 18:22:02 -0600

    Fixed spawns
    sdaf

---
+++ robo-boat_v2/robot-Rowboat/Assets/BoatHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatHealth : MonoBehaviour {
+
+    public int Health;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            Health = Health - 1;
+            Destroy(col.gameObject);
+            Debug.Log("DAMAGE");
+            Debug.Log(col.gameObject.name);
+        }
+        if (col.gameObject.tag == "Island")
+        {
+            Debug.Log("You win");
+        }
+    }
+}
--- robo-boat_v2/robot-Rowboat/Assets/Shark.cs
+++ robo-boat_v2/robot-Rowboat/Assets/Shark.cs
@@ -1,24 +1,14 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
-using NUnit.Framework.Constraints;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
-public class EvilBird : Enemy
-{
+public class Shark : Enemy {
 
     [SerializeField] private float speed;
     [SerializeField] private float jitterSpeed;
     private Vector3 target;
     private Vector3 currOffset;
     private Vector3 oldPos;
-    
-    protected override void Die()
-    {
-        base.Die();
-    }
-
 
     protected override void EnemyStart()
     {
@@ -32,8 +22,8 @@ public class EvilBird : Enemy
     {
         float step = speed * Time.deltaTime; // calculate distance to move
         transform.position = Vector3.MoveTowards(transform.position, target + currOffset, step);
+        transform.LookAt(target);
         //Debug.Log("target" + target);
-        LookAtPlayer();
         // Check if the position of the cube and sphere are approximately equal.
         if (Vector3.Distance(transform.position, target) < 0.001f)
         {
@@ -46,7 +36,7 @@ public class EvilBird : Enemy
         int count = 0;
         while (true)
         {
-            Movement();
+            AttackPlayer();
             count++;
             yield return new WaitForSeconds(0.8f + Random.Range(-0.1f, 0.4f));
         }
@@ -57,18 +47,19 @@ public class EvilBird : Enemy
     private void Movement()
     {
         Transform playerTransform = MapGenerator.Instance.player;
-        Vector3 randomPos = new Vector3(Random.Range(-1f,1f), Random.Range(-1f,1f), Random.Range(-1f,1f));
+        Vector3 randomPos = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
         float scaleFactor =
             jitterSpeed;// * Vector3.Dot((playerTransform.position - transform.position).normalized,randomPos.normalized);
         Vector3 toAdd = scaleFactor * randomPos;
         currOffset = toAdd;
     }
 
-    private void AttackPlayer() 
+    private void AttackPlayer()
     {
         Debug.Log("attacking");
         Transform playerTransform = MapGenerator.Instance.player;
         Vector3 diff = playerTransform.position - transform.position;
+        diff.y = 0;
         oldPos = target;
         target += 0.66f * diff;
     }
@@ -78,5 +69,4 @@ public class EvilBird : Enemy
         Transform playerTransform = MapGenerator.Instance.player;
         transform.LookAt(playerTransform);
     }
-    
 }
